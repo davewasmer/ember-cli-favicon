435687b2e7acfdd0ff3521e07db637a850c781c7
Dave Wasmer <davewasmer@gmail.com>
2015-11-02 11:56:03 -0700

    improve error handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* jshint node: true */
 'use strict';
 
+var util = require('util');
 var replace = require('broccoli-replace');
 var favicons = require('broccoli-favicon');
 
@@ -12,8 +13,19 @@ module.exports = {
 
   included: function(app) {
     this.options = app.favicons || {};
-    this.options.callback = function(html) {
-      htmlCache = html;
+    this.options.callback = function(result) {
+      if (typeof result === 'string') {
+        if (result.match(/^\d{3}/)) {
+          throw new Error('Error generating favicons (is your source file square?): ' + result);
+        }
+        htmlCache = result;
+      } else {
+        if (result.favicon_generation_result.result.status === 'error') {
+          throw new Error(result.favicon_generation_result.result.error_message);
+        } else {
+          throw new Error('The favicon generator API returned an unknown response: ' + util.inspect(result));
+        }
+      }
     };
   },
 
