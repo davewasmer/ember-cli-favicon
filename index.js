6705fc065e87e0b12eb01702e17777e09e1232a8
Dave Wasmer <davewasmer@gmail.com>
2016-04-13 11:47:42 -0600

    Update to broccoli favicon 1.0.0-beta.1

--- index.js
+++ index.js
@@ -3,7 +3,10 @@
 
 var util = require('util');
 var replace = require('broccoli-replace');
-var favicons = require('broccoli-favicon');
+var Favicons = require('broccoli-favicon');
+var mergeTrees = require('broccoli-merge-trees');
+
+var stew = require('broccoli-stew');
 
 var htmlCache = null;
 
@@ -12,42 +15,27 @@ module.exports = {
   name: 'ember-cli-favicon',
 
   included: function(app) {
-    this.options = app.favicons || {};
-    this.options.callback = function(result) {
-      if (typeof result === 'string') {
-        if (result.match(/^\d{3}/)) {
-          throw new Error('Error generating favicons (is your source file square?): ' + result);
-        }
-        htmlCache = result;
-      } else {
-        if (result.favicon_generation_result.result.status === 'error') {
-          throw new Error(result.favicon_generation_result.result.error_message);
-        } else {
-          throw new Error('The favicon generator API returned an unknown response: ' + util.inspect(result));
-        }
-      }
+    this.options = app.options.favicons || {};
+    this.options.htmlCallback = function(html) {
+      htmlCache = html;
     };
   },
 
   postprocessTree: function(type, tree) {
     if (type === 'all') {
+      var favicons = new Favicons(tree, this.options);
+      tree = mergeTrees([ tree, favicons ]);
       return replace(tree, {
         files: [ 'index.html' ],
         patterns: [{
           match: /<\/head>/i,
           replacement: function() {
-            return (htmlCache || '') + '\n</head>';
+            return (htmlCache || []).join('\n') + '\n</head>';
           }
         }]
       });
     }
     return tree;
-  },
-
-  treeForPublic: function(tree) {
-    if (this.options.persistCache !== false) {
-      this.options.persistentCacheDir = this.options.persistentCacheDir || 'tmp/.favicon-cache';
-    }
-    return favicons(tree || 'public', this.options);
   }
+
 };
