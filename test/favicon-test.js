45997d3e5f0b0afbb5002f7d96c45e92fcdbb6e5
Dave Wasmer <davewasmer@gmail.com>
2016-04-13 11:48:24 -0600

    switch to basic mocha tests

---
+++ test/favicon-test.js
@@ -0,0 +1,14 @@
+var cp = require('child_process');
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+
+describe('favicons', function() {
+  it('should generate favicon images and html', function() {
+    this.timeout(15000);
+    cp.execSync('ember build');
+    assert(fs.existsSync(path.join('dist', 'favicon-16x16.png')), 'Favicons were generated');
+    var index = fs.readFileSync(path.join('dist', 'index.html'), 'utf-8');
+    assert(index.indexOf('link rel="icon"'), 'Favicon html was injected');
+  });
+});
