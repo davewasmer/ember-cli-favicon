1b4ba033ce8c5a3fca97543a452d81ae64a88b6d
Dave Wasmer <davewasmer@gmail.com>
2016-04-13 19:18:30 -0600

    bump timeout for tests

--- test/favicon-test.js
+++ test/favicon-test.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 describe('favicons', function() {
   it('should generate favicon images and html', function() {
-    this.timeout(15000);
+    this.timeout(30000);
     cp.execSync('./node_modules/.bin/ember build');
     assert(fs.existsSync(path.join('dist', 'favicon-16x16.png')), 'Favicons were generated');
     var index = fs.readFileSync(path.join('dist', 'index.html'), 'utf-8');
