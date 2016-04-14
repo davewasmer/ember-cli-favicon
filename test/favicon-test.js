3af7cd8ebc57f6c5a78ce8231a0efe1dba93937b
Dave Wasmer <davewasmer@gmail.com>
2016-04-13 18:59:32 -0600

    fix test commands for CI environment

--- test/favicon-test.js
+++ test/favicon-test.js
@@ -6,7 +6,7 @@ var path = require('path');
 describe('favicons', function() {
   it('should generate favicon images and html', function() {
     this.timeout(15000);
-    cp.execSync('ember build');
+    cp.execSync('./node_modules/.bin/ember build');
     assert(fs.existsSync(path.join('dist', 'favicon-16x16.png')), 'Favicons were generated');
     var index = fs.readFileSync(path.join('dist', 'index.html'), 'utf-8');
     assert(index.indexOf('link rel="icon"'), 'Favicon html was injected');
