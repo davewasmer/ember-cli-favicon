108ba1d0a66d31454d5141757133d6b985c29638
Alexey Kulakov <uni_que@me.com>
2017-09-09 03:25:06 -0700

    restore index.js

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 /* eslint-env node */
 'use strict';
 
+const replace = require('broccoli-replace');
+const Favicons = require('broccoli-favicon');
+const mergeTrees = require('broccoli-merge-trees');
+
+let htmlCache = null;
+
 module.exports = {
-  name: 'ember-cli-favicon'
+
+  name: 'ember-cli-favicon',
+
+  included(app) {
+    this.options = app.options.favicons || {};
+    this.options.htmlCallback = function(html) {
+      htmlCache = html;
+    };
+  },
+
+  postprocessTree(type, tree) {
+    if (type === 'all') {
+      const favicons = new Favicons(tree, this.options);
+      tree = mergeTrees([ favicons, tree ]);
+      return replace(tree, {
+        files: [ 'index.html' ],
+        patterns: [{
+          match: /<\/head>/i,
+          replacement: function() {
+            return (htmlCache || []).join('\n') + '\n</head>';
+          }
+        }]
+      });
+    }
+    return tree;
+  }
+
 };
