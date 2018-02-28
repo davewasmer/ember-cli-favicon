6eb246e6013473a2f2db6f4bdb62df047ac05b23
Joren l'Ami <joren@distinct.nl>
2018-02-28 11:51:55 +0100

    fix travis to run `npm test` again and added test to keep custom sizes

--- test/favicon-test.js
+++ test/favicon-test.js
@@ -8,9 +8,11 @@ const path = require('path');
 describe('favicons', function() {
   it('should generate favicon images and html', function() {
     this.timeout(60000);
-    cp.execSync('./node_modules/.bin/ember build');
+    cp.execSync(path.join('node_modules', '.bin', 'ember') + ' build');
     assert(fs.existsSync(path.join('dist', 'favicon-16x16.png')), 'Favicons were generated');
     let index = fs.readFileSync(path.join('dist', 'index.html'), 'utf-8');
     assert(index.indexOf('link rel="icon"'), 'Favicon html was injected');
+    let stat = fs.statSync(path.join('dist', 'favicon-32x32.png'));
+    assert(stat.size == 0, 'Custom favicon size should not be overwritten');
   });
 });
